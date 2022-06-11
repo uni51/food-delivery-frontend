1ff155e57d9ded7e124f17126fd3462a3d93ceea
Koichi.Shimomura <unieye51@gmail.com>
2022-06-11 16:16:17 +0900

    3-19 ページ共通部分のLayoutコンポーネントを作成しよう

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import { Alert, Button } from "reactstrap";
+
 const index = () => {
-  return <div>Hello, Next Js</div>;
+  return (
+    <div>
+      <div>
+        <Alert color="primary">Hello Project</Alert>
+        <Button color="primary">Hello from nextjs</Button>
+      </div>
+    </div>
+  );
 };
 
 export default index;
