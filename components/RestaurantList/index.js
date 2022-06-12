f4ea48163fe4b9db8fa4da8df44e6c2d474474fc
Koichi.Shimomura <unieye51@gmail.com>
2022-06-13 08:53:01 +0900

    4-34 検索バーにおけるフィルタリング機能を実装しよう

--- components/RestaurantList/index.js
+++ components/RestaurantList/index.js
@@ -20,9 +20,12 @@ const RestaurantList = (props) => {
   const { loading, error, data } = useQuery(query);
 
   if (data) {
+    const searchQuery = data.restaurants.filter((restaurant) =>
+      restaurant.name.toLowerCase().includes(props.search)
+    );
     return (
       <Row>
-        {data.restaurants.map((res) => (
+        {searchQuery.map((res) => (
           <Col xs="6" sm="4" key={res.id}>
             <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
               <CardImg
