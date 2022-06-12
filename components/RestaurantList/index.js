4450c477a08357213e43b38de666e82646e5da2e
Koichi.Shimomura <unieye51@gmail.com>
2022-06-13 08:36:41 +0900

    4-32 APIから取得したデータでHTMLを書き換えてみよう

--- components/RestaurantList/index.js
+++ components/RestaurantList/index.js
@@ -18,49 +18,55 @@ const query = gql`
 
 const RestaurantList = () => {
   const { loading, error, data } = useQuery(query);
-  console.log(data);
-  return (
-    <Row>
-      <Col xs="6" sm="4">
-        <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
-          <CardImg
-            src="http://localhost:1337/uploads/restaurant1_97218f501c.jpeg"
-            top={true}
-            style={{ height: 250 }}
-          />
-          <CardBody>
-            <CardTitle>Italian Restaurant</CardTitle>
-            <CardTitle>イタリアンのレストランです。</CardTitle>
-          </CardBody>
-          <div className="card-footer">
-            <Link
-              href="/restaurant?id=62a5211d536abd6cc29f1abc"
-              as="/restaurant/62a5211d536abd6cc29f1abc"
-            >
-              <a className="btn btn-primary">もっと見る</a>
-            </Link>
-          </div>
-        </Card>
-      </Col>
-      <style jsx>
-        {`
-          a {
-            color: white;
-          }
-          a: link {
-            text-decoration: none;
-            color: white;
-          }
-          a: hover {
-            color: white;
-          }
-          .card-columns {
-            column-count: 3;
-          }
-        `}
-      </style>
-    </Row>
-  );
+
+  if (data) {
+    return (
+      <Row>
+        {data.restaurants.map((res) => (
+          <Col xs="6" sm="4" key={res.id}>
+            <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
+              <CardImg
+                src={`${process.env.NEXT_PUBLIC_API_URL}${res.image[0].url}`}
+                top={true}
+                style={{ height: 250 }}
+              />
+              <CardBody>
+                <CardTitle>{res.name}</CardTitle>
+                <CardTitle>{res.description}</CardTitle>
+              </CardBody>
+              <div className="card-footer">
+                <Link
+                  href={`restaurants/${res.id}`}
+                  as={`restaurants?id=${res.id}`}
+                >
+                  <a className="btn btn-primary">もっと見る</a>
+                </Link>
+              </div>
+            </Card>
+          </Col>
+        ))}
+        <style jsx>
+          {`
+            a {
+              color: white;
+            }
+            a: link {
+              text-decoration: none;
+              color: white;
+            }
+            a: hover {
+              color: white;
+            }
+            .card-columns {
+              column-count: 3;
+            }
+          `}
+        </style>
+      </Row>
+    );
+  } else {
+    return <h1>レストランが見つかりませんでした。</h1>;
+  }
 };
 
 export default RestaurantList;
