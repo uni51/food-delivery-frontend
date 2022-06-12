f0f6b679cd7e0972f01efef65381942593cc8ec1
Koichi.Shimomura <unieye51@gmail.com>
2022-06-12 09:20:39 +0900

    4-30 レストランカードをCSSでスタイリングしよう

--- components/RestaurantList/index.js
+++ components/RestaurantList/index.js
@@ -4,9 +4,13 @@ import { CardImg, Card, Col, Row, CardBody, CardTitle } from "reactstrap";
 const RestaurantList = () => {
   return (
     <Row>
-      <Col>
-        <Card>
-          <CardImg src="" />
+      <Col xs="6" sm="4">
+        <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
+          <CardImg
+            src="http://localhost:1337/uploads/restaurant1_97218f501c.jpeg"
+            top={true}
+            style={{ height: 250 }}
+          />
           <CardBody>
             <CardTitle>Italian Restaurant</CardTitle>
             <CardTitle>イタリアンのレストランです。</CardTitle>
@@ -21,6 +25,23 @@ const RestaurantList = () => {
           </div>
         </Card>
       </Col>
+      <style jsx>
+        {`
+          a {
+            color: white;
+          }
+          a: link {
+            text-decoration: none;
+            color: white;
+          }
+          a: hover {
+            color: white;
+          }
+          .card-columns {
+            column-count: 3;
+          }
+        `}
+      </style>
     </Row>
   );
 };
