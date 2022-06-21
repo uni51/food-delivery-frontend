f7b79065dd86a1af9a0a369729074c3503ec9505
Koichi.Shimomura <unieye51@gmail.com>
2022-06-21 09:31:45 +0900

    5-46 ユーザー状態をグローバルに監視してみよう

---
+++ context/AppContext.js
@@ -0,0 +1,6 @@
+import React, { createContext } from "react";
+
+//　グローバルコンテキストの作成
+const AppContext = createContext();
+
+export default AppContext;
--- pages/_app.js
+++ pages/_app.js
@@ -3,24 +3,37 @@ import App from "next/app";
 import Head from "next/head";
 import Layout from "../components/Layout";
 import withData from "../lib/apollo";
+import AppContext from "../context/AppContext";
 
 // 全てのコンポーネントの幹になる
 class MyApp extends App {
+  state = {
+    user: null,
+  };
+
+  setUser = (user) => {
+    this.setState({ user });
+  };
+
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <>
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-          />
-        </Head>
+      <AppContext.Provider
+        value={{ user: this.state.user, setUser: this.setUser }}
+      >
+        <>
+          <Head>
+            <link
+              rel="stylesheet"
+              href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+            />
+          </Head>
 
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </>
+      </AppContext.Provider>
     );
   }
 }
