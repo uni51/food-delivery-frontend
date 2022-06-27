7dc6ee716ab1e14a9ce5814b5156ca7836cbf8a7
Koichi.Shimomura <unieye51@gmail.com>
2022-06-27 16:16:53 +0900

    5-54 クッキーが残っていればそのユーザーで自動的にログインさせよう

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import Layout from "../components/Layout";
 import withData from "../lib/apollo";
 import AppContext from "../context/AppContext";
+import Cookies from "js-cookie";
 
 // 全てのコンポーネントの幹になる
 class MyApp extends App {
@@ -14,6 +15,27 @@ class MyApp extends App {
   setUser = (user) => {
     this.setState({ user });
   };
+
+  // 既にユーザーのクッキー情報が残っているかを確認する
+  componentDidMount() {
+    const token = Cookies.get("token"); // tokenの中にjwtが入っている
+
+    if (token) {
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then(async (res) => {
+        if (!res.ok) {
+          Cookies.remove("token");
+          this.setState({ user: null });
+          return null;
+        }
+        const user = await res.json();
+        this.setUser(user); // ログイン
+      });
+    }
+  }
 
   render() {
     const { Component, pageProps } = this.props;
