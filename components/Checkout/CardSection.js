3bcb2251f89ca5ebe089b9f737d507cd77dafa52
Koichi.Shimomura <unieye51@gmail.com>
2022-07-04 13:32:48 +0900

    7-75 カード情報を打ち込むためのCardSectionコンポーネントを作成しよう

--- components/Checkout/CardSection.js
+++ components/Checkout/CardSection.js
@@ -1,5 +1,22 @@
+import { CardElement } from "@stripe/react-stripe-js";
+
 const CardSection = () => {
-  return <div>Enter</div>;
+  return (
+    <div>
+      <div>
+        <label htmlFor="card-element">クレジット/デビットカード</label>
+        <div>
+          <fieldset>
+            <div className="form-row">
+              <div id="card-element" style={{ width: "100%" }}>
+                <CardElement />
+              </div>
+            </div>
+          </fieldset>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardSection;
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,8 +1,13 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { Row, Col } from "reactstrap";
 import Cart from "../components/Cart/index";
 import CheckOutForm from "../components/Checkout/CheckOutForm";
 
 const checkout = () => {
+  const stripePromise = loadStripe(
+    "pk_test_51IqSfbE0laIT5LMFVotGorZriRrjEvDa1keEmw6w5REcLaciFkD5IbZDj27niAMrEF1KZWiAUq4czZLR56EoIJxy00zFNgt5qD"
+  );
   return (
     <Row>
       <Col style={{ paddingRight: 0 }} sm={{ size: 3, order: 1, offset: 2 }}>
@@ -12,7 +17,9 @@ const checkout = () => {
         <Cart />
       </Col>
       <Col style={{ paddingLeft: 5 }} sm={{ size: 6, order: 2 }}>
-        <CheckOutForm />
+        <Elements stripe={stripePromise}>
+          <CheckOutForm />
+        </Elements>
       </Col>
     </Row>
   );
